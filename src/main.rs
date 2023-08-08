b4afdef0061582aafdabbf2c3fde570cee33f18c
haruki7049 <tontonkirikiri@gmail.com>
2023-08-09 08:50:19 +0900

    fuga

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,12 @@
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {
-    }
-}
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {
-    }
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
