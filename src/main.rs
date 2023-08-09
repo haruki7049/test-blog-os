37f5d1c1be968e4e256cc546d74ea17423ac0c8f
haruki7049 <tontonkirikiri@gmail.com>
2023-08-09 15:21:25 +0900

    piyo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 //#[no_mangle]
@@ -8,18 +9,28 @@ use core::panic::PanicInfo;
 //    loop {}
 //}
 
-static HELLO: &[u8] = b"Hello World!";
+//static HELLO: &[u8] = b"Hello World!";
+//
+//#[no_mangle]
+//pub extern "C" fn _start() -> ! {
+//    let vga_buffer = 0xb8000 as *mut u8;
+//
+//    for (i, &byte) in HELLO.iter().enumerate() {
+//        unsafe {
+//            *vga_buffer.offset(i as isize * 2) = byte;
+//            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+//        }
+//    }
+//
+//    loop {}
+//}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    use core::fmt::Write;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::WRITER.lock().write_str("Hello agein").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop {}
 }
